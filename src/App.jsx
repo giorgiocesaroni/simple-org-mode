ec0707c1a6bc0979b52db110c8cc4a43a57af3d9
g.cesaroni <g.cesaroni@reply.it>
2023-01-31 09:48:52 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [lines, setLines] = useState([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   function handleChange(event) {
+      const value = event.target.value;
+      setLines(value.split("\n"));
+   }
+
+   useEffect(() => {
+      console.log(lines);
+   }, [lines]);
+
+   return (
+      <div className="h-full p-3">
+         <h1 className="text-xl font-bold mb-8">org-mode on the web</h1>
+
+         <div className="flex border rounded overflow-hidden">
+            <div className="flex-1">
+               <textarea
+                  placeholder="Type to start..."
+                  className="w-full h-full resize-none outline-none p-4"
+                  onChange={handleChange}
+               />
+            </div>
+            <div className="bg-slate-200 flex-1 flex flex-col p-4">
+               {lines.map((line, index) => (
+                  <Parser>{line}</Parser>
+               ))}
+            </div>
+         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+   );
 }
 
-export default App
+function Parser({ children }) {
+   console.log(children[0]);
+
+   if (children[0] === "*") {
+      return <h1 className="text-xl font-bold my-4">{children.slice(2)}</h1>;
+   } else {
+      return <span>{children}</span>;
+   }
+}
+
+export default App;
