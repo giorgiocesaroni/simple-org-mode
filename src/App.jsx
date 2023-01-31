549ae64d9191c79f9252bc09d16df7f65819f752
g.cesaroni <g.cesaroni@reply.it>
2023-01-31 10:53:27 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import { useState, useEffect } from "react";
 
 function App() {
+   const [text, setText] = useState("");
    const [lines, setLines] = useState([]);
+
+   useEffect(() => {
+      setText(localStorage.getItem("text"));
+      setLines(localStorage.getItem("text")?.split("\n") ?? []);
+   }, []);
 
    function handleChange(event) {
       const value = event.target.value;
+      setText(value);
       setLines(value.split("\n"));
+
+      localStorage.setItem("text", value);
    }
 
-   useEffect(() => {
-      console.log(lines);
-   }, [lines]);
-
    return (
-      <div className="h-full p-3">
-         <h1 className="text-xl font-bold mb-8">org-mode on the web</h1>
+      <div className="h-full p-3 pb-8 flex flex-col touch-none">
+         <div className="mb-8">
+            <h1 className="text-xl font-bold">org-mode on the web</h1>
+            <p className="text-slate-400">Let's get organized!</p>
+         </div>
 
-         <div className="flex border rounded overflow-hidden">
+         <div className="flex border rounded overflow-hidden h-full">
             <div className="flex-1">
                <textarea
-                  placeholder="Type to start..."
+                  value={text}
+                  placeholder="Start typing..."
                   className="w-full h-full resize-none outline-none p-4"
                   onChange={handleChange}
                />
             </div>
-            <div className="bg-slate-200 flex-1 flex flex-col p-4">
+            <div className="bg-slate-100 overflow-y-scroll flex-1 flex flex-col p-4">
                {lines.map((line, index) => (
                   <Parser>{line}</Parser>
                ))}
@@ -34,13 +43,35 @@ function App() {
    );
 }
 
-function Parser({ children }) {
-   console.log(children[0]);
+function Parser({ children: line }) {
+   const split = line.split(/\s+/);
 
-   if (children[0] === "*") {
-      return <h1 className="text-xl font-bold my-4">{children.slice(2)}</h1>;
+   // TODO Section
+   if (split[1] === "TODO") {
+      return (
+         <div className="my-2">
+            <span className="bg-yellow-400 rounded px-1 font-semibold">
+               TODO
+            </span>{" "}
+            <span>{split.slice(2).join(" ")}</span>
+         </div>
+      );
+   }
+
+   if (split[0] === "*") {
+      return (
+         <h1 className="text-2xl font-bold my-4">{split.slice(1).join(" ")}</h1>
+      );
+   } else if (split[0] === "**") {
+      return (
+         <h1 className="text-xl font-bold my-4">{split.slice(1).join(" ")}</h1>
+      );
+   } else if (split[0] === "***") {
+      return (
+         <h1 className="text-md font-bold my-4">{split.slice(1).join(" ")}</h1>
+      );
    } else {
-      return <span>{children}</span>;
+      return <span>{line}</span>;
    }
 }
 
